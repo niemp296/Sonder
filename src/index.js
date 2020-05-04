a614c204135549e5c0edcc5ae3015052a9922ae8
Phuong Nguyen <nie.mp296@gmail.com>
2020-05-03 21:12:25 -0700

    create Sign Up Page (#33)

--- src/index.js
+++ src/index.js
@@ -4,13 +4,16 @@ import './index.css';
 import Home from './Pages/Home';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import SignUp from './Pages/Components/SignUp/SignUp';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Home />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  <Router>
+      <div>
+          <Route exact path="/" component={Home} />
+          <Route path="/sign-up" component={SignUp} />
+      </div>
+  </Router>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
