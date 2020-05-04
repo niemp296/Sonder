8b75aa99757035d325b8710960e3a13f3d33f37d
Phuong Nguyen <nie.mp296@gmail.com>
2020-05-03 21:07:53 -0700

    create Sign Up Page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import SignUp from './Pages/Components/SignUp';
 
 function App() {
+
+    const signUpPage = (
+      <Route path={SignUp}>
+        <SignUp />
+      </Route>
+    )
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        {signUpPage}
+      </Switch>
+    </Router>
   );
 }
 
--- src/Pages/Components/Header/Header.js
+++ src/Pages/Components/Header/Header.js
@@ -10,15 +10,15 @@ export default class Header extends React.Component {
             //need to set up to change this state variable later when sign in
             //and sign up page are done
             //for now can change the boolean of this variable to test for UI
-            isLoggedIn : true 
+            isLoggedIn : false 
         }
     }
 
     _beforeLogIn() {
         return (
             <ul> 
-                <img src={logo} className="Logo" alt="logo" />             
-                <li><a href="#signUp">Sign Up</a></li> 
+                <img src={logo} className="Logo" alt="logo" />        
+                <li><a href="sign-up">Sign Up</a></li> 
                 <li><a href="#signIn">Sign In</a></li> 
                 <li><a href="#help">Help</a></li>                                     
             </ul>
@@ -28,7 +28,7 @@ export default class Header extends React.Component {
     _afterLogIn() {
         return (
             <ul> 
-                <img src={logo} className="Logo" alt="logo" />             
+                <img src={logo} className="Logo" alt="logo"/>            
                 <li><a href="#myProfile">My Profile</a></li> 
                 <li><a href="#help">Help</a></li>                                     
             </ul>
