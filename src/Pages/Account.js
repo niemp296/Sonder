40b694bc387597716ce6b911c38ba118b525c480
Irenna Lumbuun <irennanicole1@gmail.com>
2020-05-10 22:38:07 -0700

    style Account page

--- src/Pages/Account.js
+++ src/Pages/Account.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Header from './Components/Header/Header';
 import axios from "axios";
-
+import "./Account.css";
 /*
 todo: 
 - get user id from props
@@ -17,41 +17,55 @@ export default class Account extends React.Component {
         super(props);
         //this.handler = this.handler.bind(this);
         this.state = {
-            id : this.props.match.params.id,
+            id : this.props.match.params.id.substring(1, this.props.match.params.id.length),
             firstName: "",
             lastName: "",
             email: "",
         }
+        this.getUserInfo();
     }
     getUserInfo = () => {
-        var id = this.state.id.substring(1,this.state.id.length)
+        var id = this.state.id;
         axios.get('http://localhost:5000/api/users/' + id.toString())
-            .then(function (response) {
+            .then((response) => {
                 // handle success
                 console.log(response);
+                console.log(response.data[0].firstname);
+                console.log(response.data[0].lastName);
+                console.log(response.data[0].email);
+                
+                var user_data = response.data[0];
+                if(response.status === 200){
+                    this.setState({
+                        firstName: user_data.firstname,
+                        lastName: user_data.lastName,
+                        email: user_data.email
+                    })
+                    console.log(this.state);
+                }
             })
             .catch(function (error) {
                 // handle error
                 console.log(error);
             })
-            .then(function () {
-                // always executed
-            });
     }
-
     render() {
-        console.log("id", this.props.match.params.id);
-        this.getUserInfo();
         return (
             <div>
                 <Header />
-                <section className="container">
-                    <h1>Welcome Back!</h1>
-                    <ul>
-                        <li>Plans</li>
-                        <li>Update Profile</li>
-                        <li>Delete Account</li>
-                    </ul>
+                <section id="account-container">
+                    <section id ="greet_user">
+                        <h1 className="greet_user">Welcome Back, </h1>
+                        <h1 className="greet_user">{this.state.firstName}</h1>
+                    </section>
+                    <section id="account-plan-list">
+                        <h1>Your travel plans:</h1>
+                    </section>
+                    <div class="btn-toolbar account-button-group" role="group" aria-label="Basic example">
+                        <button type="button" class="btn btn-outline-dark">Update Profile</button>
+                        <button type="button" class="btn btn-outline-dark">Change Password</button>
+                        <button type="button" class="btn btn-outline-danger">Delete Account</button>
+                    </div>
                 </section>
             </div>
         );
