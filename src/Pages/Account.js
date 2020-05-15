981c3d9ff30bebd7c5df60521ef1364ca0d9f7bf
Irenna Lumbuun <irennanicole1@gmail.com>
2020-05-14 17:13:15 -0700

    account page display users' plan. add author in plan document

--- src/Pages/Account.js
+++ src/Pages/Account.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Header from './Components/Header/Header';
 import axios from "axios";
 import "./Account.css";
+import Plan from './Components/Plan/Plan'
+import { Redirect } from 'react-router-dom'
 /*
 todo: 
 - get user id from props
@@ -21,6 +23,9 @@ export default class Account extends React.Component {
             firstName: "",
             lastName: "",
             email: "",
+            plans: [],
+            add_plan: false,
+            see_plan: ""
         }
         this.getUserInfo();
     }
@@ -29,19 +34,14 @@ export default class Account extends React.Component {
         axios.get('http://localhost:5000/api/users/' + id.toString())
             .then((response) => {
                 // handle success
-                console.log(response);
-                console.log(response.data[0].firstname);
-                console.log(response.data[0].lastName);
-                console.log(response.data[0].email);
-                
-                var user_data = response.data[0];
+                let user_data = response.data[0];
                 if(response.status === 200){
                     this.setState({
                         firstName: user_data.firstname,
                         lastName: user_data.lastName,
-                        email: user_data.email
+                        email: user_data.email,
+                        plans: user_data.plans
                     })
-                    console.log(this.state);
                 }
             })
             .catch(function (error) {
@@ -49,22 +49,66 @@ export default class Account extends React.Component {
                 console.log(error);
             })
     }
+
+    //this method is called when user clicks the addplan button
+    addPlan = () =>{
+        this.setState({
+            add_plan: true
+        })
+    }
+
+    //this method is called when the user clicks on one of the plan
+    seePlan = (plan_id) => {
+        this.setState({
+            see_plan: plan_id
+        })
+        console.log(plan_id);
+    }
+
+    renderPlans(){
+        if(this.state.plans.length === 0) return; //user has no plans
+
+        return(<ul id="user-plans-group">{
+            this.state.plans.map(
+                plan => 
+                <li onClick = {() => this.seePlan(plan)}> <Plan id={plan}></Plan>
+                </li>)} 
+                </ul>);
+    }
+    /*
+    TODO: redirect add plans to map
+    */
     render() {
+        if(this.state.add_plan){
+            return <Redirect to ="/"></Redirect>
+        }
+        if(this.state.see_plan !== ""){
+            console.log(this.state.see_plan)
+            let path = '/plan-trip:' + this.state.id + ':' + this.state.see_plan;
+            return <Redirect to = {path} />
+        }
         return (
             <div>
                 <Header />
                 <section id="account-container">
                     <section id ="greet_user">
-                        <h1 className="greet_user">Welcome Back, </h1>
+                        <h1 className="greet_user">Hello,</h1>
                         <h1 className="greet_user">{this.state.firstName}</h1>
                     </section>
                     <section id="account-plan-list">
-                        <h1>Your travel plans:</h1>
+                        <h1>Your travel plans</h1>
+                        <button 
+                        className="btn btn-lg btn-block btn-outline-dark"
+                        onClick = {this.addPlan
+                        }>
+                            Add a plan
+                        </button>
+                        {this.renderPlans()}
                     </section>
-                    <div class="btn-toolbar account-button-group" role="group" aria-label="Basic example">
-                        <button type="button" class="btn btn-outline-dark">Update Profile</button>
-                        <button type="button" class="btn btn-outline-dark">Change Password</button>
-                        <button type="button" class="btn btn-outline-danger">Delete Account</button>
+                    <div className="btn-toolbar account-button-group" role="group" aria-label="account-btn-settings">
+                        <button type="button" className="btn btn-outline-dark">Update Profile</button>
+                        <button type="button" className="btn btn-outline-dark">Log Out</button>
+                        <button type="button" className="btn btn-outline-danger">Delete Account</button>
                     </div>
                 </section>
             </div>
