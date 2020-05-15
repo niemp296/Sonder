beecb7f52461201df8a473afe3548b4376cc02b2
Irenna Lumbuun <irennanicole1@gmail.com>
2020-05-14 23:08:53 -0700

    handle re-render on props change

--- src/Pages/Components/Planner/Planner-Main/Main.jsx
+++ src/Pages/Components/Planner/Planner-Main/Main.jsx
@@ -6,12 +6,10 @@ import axios from 'axios';
 //this class contains the list of all activities and
 //render the time of day + activities
 export default class Main extends React.Component {
-    static propTypes = {
-        //contains prop variables
-    }
     constructor(props) {
         super(props);
         this.state = {
+            cost: 0.0,
             time: ["morning", "afternoon", "evening"],
             morning:[],
             afternoon: [],
@@ -20,22 +18,23 @@ export default class Main extends React.Component {
           console.log("clicked day ", this.props.day);
           this.getEachLocations();
     }
-
-    static defaultProps = {
-        // contains default props
-    }
+    
     getEachLocations = () => {
         this.state.time.map(t =>
             this.props.locations[this.props.day][t].map(location => 
                 axios.get('http://localhost:5000/api/locations/' + location)
                 .then((response) =>{
                     //handle success
-                    console.log(t);
                     const location_data = response.data;
+                    const append_to_array = this.state[t].concat(location_data);
                     this.setState({
-                        [t]: [this.state.t, location_data]
+                        [t]: append_to_array
                     })
-                    console.log(this.state);
+                    //add to total cost
+                    this.setState({
+                        cost: this.state.cost + location_data.spending
+                    })
+
                 })
                 .catch(function(error){
                     console.log(error);
@@ -44,10 +43,25 @@ export default class Main extends React.Component {
         )
     }
 
+    componentDidUpdate(prevProps){
+        if(prevProps.day !== this.props.day){
+            //user wants to see plan for different day
+            this.setState({
+                cost : 0,
+                morning:[],
+                afternoon: [],
+                evening: []
+            })
+            this.getEachLocations();
+        }
+    }
+
     render() {
         return (
             <div>
-                <h1 id="day-estimated-cost">Estimated cost: </h1>
+                <h5 id="day-estimated-cost">
+                    Estimated cost for day {this.props.day + 1}: ${this.state.cost}
+                    </h5>
                 {this.state.time.map(t =>
                     <div>
                         <h1 id="day-time">{t}</h1>
