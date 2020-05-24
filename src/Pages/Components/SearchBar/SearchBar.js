b21950267f0e62d156021f6b12086e4f1fc0ce4a
Kaung Myat Aung (Eddie) <36400660+eddiekaung@users.noreply.github.com>
2020-05-24 00:33:12 -0700

    Setup SearchList API for Map #55

--- src/Pages/Components/SearchBar/SearchBar.js
+++ src/Pages/Components/SearchBar/SearchBar.js
@@ -1,53 +1,67 @@
 import React, { Component } from 'react';
 import SearchList from '../SearchList/SearchList';
-
+import list from '../../../TestData/search.json'
+import Grid from '@material-ui/core/Grid';
+import Map from '../Map/Map'
+import axios from 'axios';
 
 class SearchBar extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            data: [],
             filtered: []
         }
     }
-    componentDidMount() {
+    async componentDidMount() {
         this.setState({
-          filtered: this.props.items
+          data: (await axios.get('http://localhost:5000/api/locations/')
+                    .then((response) => response.data)),
+          filtered: []
         });
+        console.log(this.state.data)
       }
       
-      componentWillReceiveProps(nextProps) {
-        this.setState({
-          filtered: nextProps.items
-        });
-      }
+    //   componentWillReceiveProps(nextProps) {
+    //     this.setState({
+    //       filtered: nextProps.items
+    //     });
+    //   }
 
-      handleChange = (e) => {
+    handleChange = async(e) => {
         let currentList = [];
         let newList = [];
         
         let search = e.target.value;
         if (search !== "") {
-            currentList = this.props.items;
+            currentList = this.state.data;
             newList = currentList.filter(item => {
-            //convert list of items and search to lowercase so capitalization doesn't matter
-            return item.name.toLowerCase().includes(search.toLowerCase());
-            });
+                            return item.name.toLowerCase().includes(search.toLowerCase());
+                        })
         } else {
-            newList = this.props.items;
-            }
-            this.setState({
-                filtered: newList
-            });
+            newList = [];
         }
-    
+        this.setState({
+            filtered: newList
+        });
+    }
+
     render() {
         return (
             <div>
-                <form className="form-inline d-flex justify-content-center md-form form-md mt-0">
-                <input className="form-control form-control-sm w-75" type="text" onChange={this.handleChange} placeholder="Search"
-                    aria-label="Search" />
-                </form>
-                <SearchList items={this.state.filtered} />
+                <Grid container spacing={3}>
+                    <Grid item xs={3}>
+                        <form className="form-inline d-flex justify-content-center md-form form-md mt-0">
+                        <input className="form-control form-control-sm w-75" type="text" onChange={this.handleChange} placeholder="Search"
+                            aria-label="Search" />
+                        </form>
+                        <SearchList items={this.state.filtered} />
+                    </Grid>
+                    <Grid item xs={9}>
+                        <Map items={this.state.filtered}/> 
+                    </Grid>
+                </Grid>
+                
             </div>
         )
     }
