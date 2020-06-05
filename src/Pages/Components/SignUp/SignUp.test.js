2ac161684bfa6bdb67f93608e779a3ed6bf4b5ba
Irenna Lumbuun <irennanicole1@gmail.com>
2020-06-05 01:57:37 -0700

    add more test to sign in and signup

--- src/Pages/Components/SignUp/SignUp.test.js
+++ src/Pages/Components/SignUp/SignUp.test.js
@@ -54,3 +54,29 @@ describe('Input box is working', () =>{
         expect(wrapper.state('advertiser')).toEqual(true);
     })
 })
+
+describe('test form errors', () =>{
+    let wrapper = mount(<SignUp />);
+    const preventDefault = jest.fn();
+
+    it('error first name', () =>{
+        wrapper.find('input[name="firstName"]').simulate('change', {target: {name: 'firstName', value: ''}});
+        expect(wrapper.state('formErrors').firstName).toEqual("minimum 1 characaters required");
+    })
+    it('error last name', () =>{
+        wrapper.find('input[name="lastName"]').simulate('change', {target: {name: 'lastName', value: ''}});
+        expect(wrapper.state('formErrors').lastName).toEqual("minimum 1 characaters required");
+    })
+    it('error email', () =>{
+        wrapper.find('input[name="email"]').simulate('change', {target: {name: 'email', value: '..'}});
+        expect(wrapper.state('formErrors').email).toEqual("invalid email address");
+    })
+    it('error password', () =>{
+        wrapper.find('input[name="password"]').simulate('change', {target: {name: 'password', value: 'x'}});
+        expect(wrapper.state('formErrors').password).toEqual("minimum 6 characaters required");
+    })
+    it('error confirm password', () =>{
+        wrapper.find('input[name="confirmPassword"]').simulate('change', {target: {name: 'confirmPassword', value: 'forgot'}});
+        expect(wrapper.state('formErrors').confirmPassword).toEqual("password does not match");
+    })
+})
